e4acc3b528823d2d2cea007babb30b51be53769d
hasibul hossain <hasibulhossain157@gmail.com>
2024-01-12 11:42:54 +0600

    third commit

--- text.js
+++ text.js
@@ -1,3 +1,4 @@
 let text1 = document.createTextNode("hello dunia");
 document.body.appendChild(text1);
-// document.body.appendChild(text1);
+text1 =  document.createTextNode(" hello dunia tomara ami bidai");
+document.getElementById("demo").appendChild( text1);
